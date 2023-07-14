603e3425a5d6de32ed2d5c3818c3a94267a5acd2
Jack Chenhall <chenhall_j@hotmail.co.uk>
2023-07-14 23:35:35 +0100

    removed normal car

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./style.css";
 
 import { CarShow } from "./CarShow";
 
 const App = () => {
-  const [car, setCar] = useState("1");
   return (
-    <>
-      <div className="buttonGroup">
-        <button onClick={() => setCar("2")} className="button1">
-          Sport
-        </button>
-
-        <button onClick={() => setCar("1")} className="button2">
-          Volvo
-        </button>
-      </div>
-
-      <Suspense fallback={null}>
-        <Canvas shadows>
-          <CarShow car={car} />
-        </Canvas>
-      </Suspense>
-    </>
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
   );
 };
 export default App;
--- src/Car.js
+++ src/Car.js
@@ -4,29 +4,14 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Mesh } from "three";
 
 export function Car(props) {
-  const sportsCar = "models/car/scene.gltf";
-  const volvo = "models/volvo/scene.gltf";
-
-  // const test = useEffect(() => {
-  //   if (props === "1") {
-  //     return volvo;
-  //   } else {
-  //     return sportsCar;
-  //   }
-  // }, []);
-
   const gltf = useLoader(
     GLTFLoader,
 
-    process.env.PUBLIC_URL + `${props.car === "1" ? volvo : sportsCar}`
+    process.env.PUBLIC_URL + "models/volvo/scene.gltf"
   );
 
   useEffect(() => {
-    if (props.car === "1") {
-      gltf.scene.scale.set(0.9, 0.9, 0.9);
-    } else {
-      gltf.scene.scale.set(0.005, 0.005, 0.005);
-    }
+    gltf.scene.scale.set(0.9, 0.9, 0.9);
 
     gltf.scene.position.set(0, -0.0035, 0);
     gltf.scene.traverse((object) => {
@@ -40,20 +25,13 @@ export function Car(props) {
 
   useFrame((state, delta) => {
     let t = state.clock.getElapsedTime() * 2;
-    //group.children[0 , 2 , 4 , 6]
-    //group.children[16 , 17 , 18 , 19]
+
     let group = gltf.scene.children[0].children[0].children[0];
-    if (props.car === "1") {
-      group.children[16].rotation.x = t;
-      group.children[17].rotation.x = t;
-      group.children[18].rotation.x = t;
-      group.children[19].rotation.x = t;
-    } else {
-      group.children[0].rotation.x = t;
-      group.children[2].rotation.x = t;
-      group.children[4].rotation.x = t;
-      group.children[6].rotation.x = t;
-    }
+
+    group.children[16].rotation.x = t;
+    group.children[17].rotation.x = t;
+    group.children[18].rotation.x = t;
+    group.children[19].rotation.x = t;
   });
 
   return <primitive object={gltf.scene} />;
